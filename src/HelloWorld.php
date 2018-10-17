5285cdacc84c3ef7a630baaec4a9e632a8aad1bc
schroet <schroedt@live.de>
2018-10-17 09:01:43 +0200

    initial

---
+++ src/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+ * (c) schroedt <schroedt@live.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace S6t\HelloWorld;
+
+class HelloWorld
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
